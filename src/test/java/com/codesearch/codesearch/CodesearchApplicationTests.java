e5baca3c4e28c053f40a55ce4170b1d1829e7817
cheenoxd <zesanrahim@gmail.com>
2025-10-01 13:19:35 -0400

    Initial commit

---
+++ src/test/java/com/codesearch/codesearch/CodesearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesearch.codesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
