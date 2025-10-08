228eb1e51e5d9182ec6ae121a99b205e3478109c
cheenoxd <zesanrahim@gmail.com>
2025-10-07 20:09:14 -0400

    fix(db): add crud + remove password

--- src/main/java/com/codesearch/codesearch/models/User.java
+++ src/main/java/com/codesearch/codesearch/models/User.java
@@ -4,15 +4,13 @@ import jakarta.persistence.*;
 import java.util.UUID;
 
 @Entity
-@Table(name = "app_user") 
+@Table(name = "app_user")
 public class User {
 
     @Id
     @GeneratedValue
     private UUID id;
-
     private String username;
-    private String password;
     private String email;
     private String token;
 
@@ -28,13 +26,6 @@ public class User {
     }
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getEmail() {
