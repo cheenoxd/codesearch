b6589fed911d1da5e363d3fef6c5a35969012f42
cheenoxd <zesanrahim@gmail.com>
2025-10-11 21:35:43 -0400

    feat(es): fix errors

--- src/main/java/com/codesearch/codesearch/repositories/GithubRepository.java
+++ src/main/java/com/codesearch/codesearch/repositories/GithubRepository.java
@@ -1,20 +1,19 @@
 package com.codesearch.codesearch.repositories;
 
-
 import com.codesearch.codesearch.models.Github;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface GithubRepository extends ElasticsearchRepository<Github, String> {
+
     List<Github> findByRepoName(String repoName);
     List<Github> findByFilePath(String filePath);
-    List<Github> findByName(String fileName);
+    List<Github> findByFilename(String filename);
     List<Github> findByLanguage(String language);
-    List<Github> findByContent(String content);
-    Optional<Github> contentByHash(String contentHash);
-
-
+    List<Github> findByContentContaining(String content);
+    Optional<Github> findByContentHash(String contentHash);
 }
-
